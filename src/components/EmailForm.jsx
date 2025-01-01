26fad8958fe1f6c243a0d932a33f9e0ffb2e49e7
minjeeki <minjeeki@student.42seoul.kr>
2025-01-01 16:49:51 +0900

    Delete Console.log

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -13,11 +13,6 @@ const EmailForm = ({ isOpen, onClose }) => {
   });
   useEffect(() => {
     emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
-    console.log({
-      publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
-      serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID
-    })
   }, []);
   
   const handleSubmit = async (e) => {
